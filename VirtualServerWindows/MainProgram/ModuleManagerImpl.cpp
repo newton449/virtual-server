40888eaedec28730f63e613683b67d3f2f753620
Sheng Wang <swang61@syr.edu>
2014-03-25 18:14:24 -0400

    Changed structure of ModuleManagerImpl to separate it from Windows-only codes.

--- VirtualServerWindows/MainProgram/ModuleManagerImpl.cpp
+++ VirtualServerWindows/MainProgram/ModuleManagerImpl.cpp
@@ -97,27 +97,3 @@ void ModuleManagerImpl::addModuleObjectFactory(string moduleName, IModuleObjectF
 	// TODO design
 	factoryMap[moduleName] = moduleFactory;
 }
-
-#include <locale>
-#include <codecvt>
-#include <string>
-
-IModuleObjectFactory* ModuleManagerImpl::loadDllAndGetModuleFactory(string path){
-	std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
-	std::wstring wpath = converter.from_bytes(path);
-
-	HINSTANCE hGetProcIDDLL = LoadLibrary(wpath.c_str());
-
-	if (!hGetProcIDDLL) {
-		std::cout << "could not load the dynamic library" << std::endl;
-		return NULL;
-	}
-
-	loadModuleAndReturnFactory func = (loadModuleAndReturnFactory)GetProcAddress(hGetProcIDDLL, "loadModuleAndReturnFactory");
-	if (!func) {
-		std::cout << "could not locate the function" << std::endl;
-		return NULL;
-	}
-
-	return func(MainObjectFactoryImpl::getInstance());
-}
