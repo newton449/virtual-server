12c149e6f302f7c41c1687f132649a6488ea7861
Sheng Wang <swang61@live.com>
2014-04-23 11:18:37 -0400

    Added netbeans configs for x86 and x64.

---
+++ VirtualServerLinux/Installer/nbproject/Makefile-Release_x86.mk
@@ -0,0 +1,108 @@
+#
+# Generated Makefile - do not edit!
+#
+# Edit the Makefile in the project folder instead (../Makefile). Each target
+# has a -pre and a -post target defined where you can add customized code.
+#
+# This makefile implements configuration specific macros and targets.
+
+
+# Environment
+MKDIR=mkdir
+CP=cp
+GREP=grep
+NM=nm
+CCADMIN=CCadmin
+RANLIB=ranlib
+CC=gcc
+CCC=g++
+CXX=g++
+FC=gfortran
+AS=as
+
+# Macros
+CND_PLATFORM=GNU-Linux-x86
+CND_DLIB_EXT=so
+CND_CONF=Release_x86
+CND_DISTDIR=dist
+CND_BUILDDIR=build
+
+# Include project Makefile
+include Makefile
+
+# Object Directory
+OBJECTDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}
+
+# Object Files
+OBJECTFILES= \
+	${OBJECTDIR}/_ext/1278082062/HttpServletsImpl.o \
+	${OBJECTDIR}/_ext/1278082062/ModuleObjectFactoryImpl.o \
+	${OBJECTDIR}/_ext/1278082062/ServletFactoryImpl.o \
+	${OBJECTDIR}/_ext/768354932/ILogger.o \
+	${OBJECTDIR}/_ext/147134308/FileSystem_Linux.o
+
+
+# C Compiler Flags
+CFLAGS=
+
+# CC Compiler Flags
+CCFLAGS=-m32
+CXXFLAGS=-m32
+
+# Fortran Compiler Flags
+FFLAGS=
+
+# Assembler Flags
+ASFLAGS=
+
+# Link Libraries and Options
+LDLIBSOPTIONS=
+
+# Build Targets
+.build-conf: ${BUILD_SUBPROJECTS}
+	"${MAKE}"  -f nbproject/Makefile-${CND_CONF}.mk ../../FinalTarget/Installer/Installer_x86.so
+
+../../FinalTarget/Installer/Installer_x86.so: ${OBJECTFILES}
+	${MKDIR} -p ../../FinalTarget/Installer
+	${LINK.cc} -o ../../FinalTarget/Installer/Installer_x86.so ${OBJECTFILES} ${LDLIBSOPTIONS} -shared -fPIC
+
+${OBJECTDIR}/_ext/1278082062/HttpServletsImpl.o: ../../VirtualServerWindows/Installer/HttpServletsImpl.cpp 
+	${MKDIR} -p ${OBJECTDIR}/_ext/1278082062
+	${RM} "$@.d"
+	$(COMPILE.cc) -O2 -Werror -std=c++11 -fPIC  -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/_ext/1278082062/HttpServletsImpl.o ../../VirtualServerWindows/Installer/HttpServletsImpl.cpp
+
+${OBJECTDIR}/_ext/1278082062/ModuleObjectFactoryImpl.o: ../../VirtualServerWindows/Installer/ModuleObjectFactoryImpl.cpp 
+	${MKDIR} -p ${OBJECTDIR}/_ext/1278082062
+	${RM} "$@.d"
+	$(COMPILE.cc) -O2 -Werror -std=c++11 -fPIC  -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/_ext/1278082062/ModuleObjectFactoryImpl.o ../../VirtualServerWindows/Installer/ModuleObjectFactoryImpl.cpp
+
+${OBJECTDIR}/_ext/1278082062/ServletFactoryImpl.o: ../../VirtualServerWindows/Installer/ServletFactoryImpl.cpp 
+	${MKDIR} -p ${OBJECTDIR}/_ext/1278082062
+	${RM} "$@.d"
+	$(COMPILE.cc) -O2 -Werror -std=c++11 -fPIC  -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/_ext/1278082062/ServletFactoryImpl.o ../../VirtualServerWindows/Installer/ServletFactoryImpl.cpp
+
+${OBJECTDIR}/_ext/768354932/ILogger.o: ../../VirtualServerWindows/interfaces/ILogger.cpp 
+	${MKDIR} -p ${OBJECTDIR}/_ext/768354932
+	${RM} "$@.d"
+	$(COMPILE.cc) -O2 -Werror -std=c++11 -fPIC  -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/_ext/768354932/ILogger.o ../../VirtualServerWindows/interfaces/ILogger.cpp
+
+${OBJECTDIR}/_ext/147134308/FileSystem_Linux.o: ../MainProgram/FileSystem_Linux.cpp 
+	${MKDIR} -p ${OBJECTDIR}/_ext/147134308
+	${RM} "$@.d"
+	$(COMPILE.cc) -O2 -Werror -std=c++11 -fPIC  -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/_ext/147134308/FileSystem_Linux.o ../MainProgram/FileSystem_Linux.cpp
+
+# Subprojects
+.build-subprojects:
+
+# Clean Targets
+.clean-conf: ${CLEAN_SUBPROJECTS}
+	${RM} -r ${CND_BUILDDIR}/${CND_CONF}
+	${RM} ../../FinalTarget/Installer/Installer_x86.so
+
+# Subprojects
+.clean-subprojects:
+
+# Enable dependency checking
+.dep.inc: .depcheck-impl
+
+include .dep.inc
